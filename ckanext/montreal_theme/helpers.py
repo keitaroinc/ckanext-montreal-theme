fc1a7dc035d918e75c274ec34fcc8b6837fcf941
Konstantin Sivakov <konstantin.sivakov@gmail.com>
2022-11-21 11:52:31 +0100

    Initial commit

---
+++ ckanext/montreal_theme/helpers.py
@@ -0,0 +1,51 @@
+from ckan import model
+import ckan.plugins as p
+from ckan.plugins import toolkit as tk
+
+
+g = tk.g
+
+def get_all_organizations(include_dataset_count=True):
+    '''Return a list of organizations that the current user has the specified
+    permission for.
+    '''
+    context = {'user': g.user}
+    data_dict = {
+        'include_dataset_count': include_dataset_count,
+        'all_fields': True}
+    return tk.get_action('organization_list')(context, data_dict)
+
+
+def get_latest_datasets():
+    '''Return a list of the latest datasets that the current user has the specified
+    permission for.
+    '''
+    context = {'user': g.user}
+    data_dict = {'sort': 'metadata_modified desc', 'rows': 4, 'include_private': True}
+
+    datasets = tk.get_action('package_search')(context, data_dict)
+    return datasets.get('results', [])
+
+
+def get_all_groups(include_dataset_count=True):
+    '''Return a list of organizations that the current user has the specified
+    permission for.
+    '''
+    context = {'user': g.user}
+    data_dict = {
+        'include_dataset_count': include_dataset_count,
+        'all_fields': True}
+    return tk.get_action('group_list')(context, data_dict)
+
+
+def get_showcases(num=6):
+    '''Return a list of showcases'''
+    return tk.get_action("ckanext_showcase_list")() or []
+
+
+def get_value_from_showcase_extras(extras, key):
+    value = ''
+    for item in extras:
+        if item.get('key') == key:
+            value = item.get('value', '')
+    return value
---
+++ ckanext/montreal_theme/plugin.py
@@ -0,0 +1,47 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+from ckanext.montreal_theme.blueprint import frontend
+from ckanext.montreal_theme import helpers as h
+
+class MontrealThemePlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IBlueprint)
+    plugins.implements(plugins.ITemplateHelpers)
+    plugins.implements(plugins.IFacets)
+    plugins.implements(plugins.IGroupForm, inherit=True)
+
+
+    # IConfigurer
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('assets', 'montreal_theme')
+
+    def get_blueprint(self):
+        # Register the new blueprint
+        return [frontend]
+
+    # ITemplateHelpers
+    def get_helpers(self):
+        return {
+            'all_organizations': h.get_all_organizations,
+            'all_groups': h.get_all_groups,
+            'latest_datasets': h.get_latest_datasets,
+            'get_showcases': h.get_showcases,
+            'get_value_from_showcase_extras': h.get_value_from_showcase_extras
+        }
+
+    # IFacets
+    def dataset_facets(self, facets_dict, package_type):
+        facets_dict['groups'] = toolkit._('Collections')
+        return facets_dict
+
+    def group_facets(self, facets_dict, group_type, package_type):
+        return facets_dict
+
+    def organization_facets(self, facets_dict, organization_type, package_type):
+        return facets_dict
+
+    # IGroupForm
+    def group_types(self):
+        return ['collections']
